23fedbc5d71402abbaa5bc8d2f75400ccd45d530
hello <hello@ifnot.cc>
2018-11-28 10:46:36 +0800

    fix init.sh

--- init.sh
+++ init.sh
@@ -4,8 +4,12 @@ PWD=$(pwd)
 
 echo "link .files/* => ~ ..."
 
-for i in $(ls -A -I init.sh -I .git);
+for i in $(ls -A);
 do
+    if [[ "$i" == ".git" || "$i" == "init.sh" ]]
+    then
+        continue
+    fi
     test -e ~/$i && echo "backup original file $i => $i~" && mv ~/$i ~/$i~
     echo "link $PWD/$i => ~/$i"
     ln -s $PWD/$i ~
